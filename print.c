a7e256f1fafd5821ab91bf721bb50100ba95c5e0
Marcelo Pinto <xmrcl0@gmail.com>
2017-09-12 00:38:17 -0300

    refactor: Melhoraria no nome das variávies

--- print.c
+++ print.c
@@ -9,53 +9,53 @@
 #include "utils.h"
 
 void
-print_path (float ***d, int **p, size_t n, float l, int m)
+print_path (float ***distance, int **path, size_t num_city, float len, int mode)
 {
   int i;
 
 
-  if (m == 0)
+  if (mode == 0)
   {
-    for (i = 0; i < n; i++)
-      printf ("d[%d][%d]=%.2f, ", (*p)[i], (*p)[i + 1], (*d)[(*p)[i]][(*p)[i + 1]]);
-    printf ("distance=%.2f\n", l);
+    for (i = 0; i < num_city; i++)
+      printf ("d[%d][%d]=%.2f, ", (*path)[i], (*path)[i + 1], (*distance)[(*path)[i]][(*path)[i + 1]]);
+    printf ("distance=%.2f\n", len);
   }
 
-  else if (m == 1)
+  else if (mode == 1)
   {
-    for (i = 0; i <= n; i++)
-      printf ("%d,", (*p)[i]);
-    printf ("%.2f\n", l);
+    for (i = 0; i <= num_city; i++)
+      printf ("%d,", (*path)[i]);
+    printf ("%.2f\n", len);
   }
 
-  else if (m == 2)
+  else if (mode == 2)
   {
-    for (i = 0; i <= n; i++)
-      printf ("%4d", (*p)[i]);
-    printf ("\t%4.2f\n", l);
+    for (i = 0; i <= num_city; i++)
+      printf ("%4d", (*path)[i]);
+    printf ("\t%4.2f\n", len);
   }
 
   // Report specific option
-  else if (m == 3)
+  else if (mode == 3)
   {
     printf ("\nsequence:");
-    for (i = 0; i <= n; i++)
-      printf ("%3d", (*p)[i]);
-    printf ("\ndistance: %.2f\n", l);
+    for (i = 0; i <= num_city; i++)
+      printf ("%3d", (*path)[i]);
+    printf ("\ndistance: %.2f\n", len);
   }
 }
 
 
 void
-print_dist (float ***d, size_t n)
+print_dist (float ***distance, size_t num_city)
 {
   int i, j;
 
   printf ("Distance matrix:\n");
-  for (i = 0; i < n; i++)
+  for (i = 0; i < num_city; i++)
   {
-    for (j = 0; j < n; j++)
-      printf ("%6.2f", (*d)[i][j]);
+    for (j = 0; j < num_city; j++)
+      printf ("%6.2f", (*distance)[i][j]);
     printf ("\n");
   }
   printf ("\n");
@@ -63,15 +63,15 @@ print_dist (float ***d, size_t n)
 
 
 void
-print_cord (float ***v, size_t n)
+print_cord (float ***coord, size_t num_city)
 {
   int i, j;
 
   printf ("\nCities coordinates:\n");
-  for (i = 0; i < n; i++)
+  for (i = 0; i < num_city; i++)
   {
     for (j = 0; j < 2; j++)
-      printf ("%.4f ", (*v)[i][j]);
+      printf ("%.4f ", (*coord)[i][j]);
     printf ("\n");
   }
   printf ("\n");
@@ -79,22 +79,22 @@ print_cord (float ***v, size_t n)
 
 
 void
-print_repo (float **c, float **d, int *min_p, int n, float min_l, long double inter, int mode)
+print_repo (float **coord, float **distance, int *min_path, int num_city, float min_len, long double inter, int mode)
 {
   // Print number of paths
   printf ("\n\n\nResults:\n");
-  printf ("\nnumber of cities = %d\n", n);
-  printf ("possible paths   = %.0Lf\n", factorial (n - 1) / 2);
+  printf ("\nnumber of cities = %d\n", num_city);
+  printf ("possible paths   = %.0Lf\n", factorial (num_city - 1) / 2);
   printf ("simulated paths  = %.0Lf\n", inter);
 
   // Print cities coordinates
-  print_cord (&c, n);
+  print_cord (&coord, num_city);
 
   // Print distance matrix
-  print_dist (&d, n);
+  print_dist (&distance, num_city);
 
   // Print minimal path found
   printf ("Minimal path found:\n");
-  print_path (&d, &min_p, n, min_l, 3);
+  print_path (&distance, &min_path, num_city, min_len, 3);
   printf ("\n");
 }
