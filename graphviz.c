abb3fabead60caa267953ce1a4605dd788be44a7
Brian K <AlpacaRotorvator@users.noreply.github.com>
2017-10-17 19:33:44 -0200

    Output em formato dot completo e funcionando razoavelmente

--- graphviz.c
+++ graphviz.c
@@ -4,13 +4,20 @@
 void gen_graphviz (float ** c, int * m, int n) {
     FILE * outfile = fopen("minpath.dot", "w");
 
-    fprintf(outfile, "digraph minpath {\n");
+    fprintf(outfile, "digraph minpath {\n\
+  overlap=\"scale\";\n\
+  splines=\"true\";\n");
 
     for(int i = 0; i < n; i++) {
-	fprintf(outfile, "\"%d\" [pos=\"%f,%f\"];\n", i, c[i][0], c[i][1]);
-
+	fprintf(outfile, "  \"%d\" [pos=\"%f,%f!\"];\n", i, c[i][0], c[i][1]);
     }
 
+    for(int i = 0; i < n - 1; i++) {
+	fprintf(outfile, "  \"%d\" -> \"%d\";\n", m[i], m[i + 1]);
+    }
+    
+    fprintf(outfile, "  \"%d\" -> \"%d\";\n", m[n - 1], m[0]);
+    
     fprintf(outfile, "}");
 
     fclose(outfile);
